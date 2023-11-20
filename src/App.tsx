419b12d447c4b80a7f7e64c9c2267addc39cdfcf
Franco Itria <francoitria01@gmail.com>
2023-11-20 19:33:54 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Chronometer from "./components/Chronometer";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <Chronometer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/home/resumen" element={<Home />} />
+        <Route path="/home/miSalud" element={<Home />} />
+        <Route path="/home/turnos" element={<Home />} />
+        {/* Rutas dinamicas para el calentamiento y rutina */}
+        {/* Ruta para la suscripcion */}
+      </Routes>
     </>
   );
 }
