2c09aa7f47de60c0c49ded4bd3a0f7f4fb0144de
fran-itria <francoitria01@gmail.com>
2024-01-12 22:25:40 -0300

    Added new component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Header from "./components/Header/Header";
+import FormLogin from "./components/Session/login/FormLogin";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<FormLogin />} />
         <Route path="/home" element={<Home />} />
         <Route path="/home/resumen" element={<Home />} />
         <Route path="/home/miSalud" element={<Home />} />
