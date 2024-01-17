043e3196fafce42700cba45d7dbc1d09e56ef73b
Franco Itria <francoitria01@gmail.com>
2024-01-17 18:19:37 -0300

    A single type onChange

--- src/services/typeServices.ts
+++ src/services/typeServices.ts
@@ -1,18 +1,13 @@
 import { InputsLogin, InputsRegister } from "../types";
 import { NavigateFunction } from "react-router-dom";
 
-export type onChangeLoginProps = {
-  event: React.ChangeEvent<HTMLInputElement>;
-  setInputs: React.Dispatch<React.SetStateAction<InputsLogin | undefined>>;
-};
-
 export type submitProps = {
   event: React.FormEvent<HTMLFormElement>;
   inputs: InputsLogin | undefined;
   navigate: NavigateFunction;
 };
 
-export type onChangeRegisterProps = {
+export type onChangeProps = {
   event: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>
-  setInputs: React.Dispatch<React.SetStateAction<InputsRegister | undefined>>
+  setInputs: React.Dispatch<React.SetStateAction<InputsLogin | InputsRegister | undefined>>
 }
