02d3315b1f264d22b11571f41992dcee856d1e9a
Franco Itria <francoitria01@gmail.com>
2024-01-17 20:26:40 -0300

    Added general type Inputs, one submit type

--- src/services/typeServices.ts
+++ src/services/typeServices.ts
@@ -1,13 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { InputsLogin, InputsRegister } from "../types";
-import { NavigateFunction } from "react-router-dom";
-
-export type submitProps = {
-  event: React.FormEvent<HTMLFormElement>;
-  inputs: InputsLogin | undefined;
-  navigate: NavigateFunction;
-};
+import { Location, NavigateFunction } from "react-router-dom";
 
 export type onChangeProps = {
   event: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>
   setInputs: React.Dispatch<React.SetStateAction<InputsLogin | InputsRegister | undefined>>
+}
+
+type Inputs = InputsLogin | InputsRegister | undefined
+
+export type registerProps = {
+  inputs: Inputs,
+  url: Location<any>
+}
+
+export type onSubmitProps = {
+  event: React.FormEvent<HTMLFormElement>;
+  inputs: Inputs;
+  navigate: NavigateFunction;
+  url?: Location<any>,
 }
