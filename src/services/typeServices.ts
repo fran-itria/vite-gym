a9e4937a0981c5ddcb6fb447d9e1aabc7b56159c
Franco Itria <francoitria01@gmail.com>
2024-01-17 17:58:50 -0300

    Added new type

--- src/services/typeServices.ts
+++ src/services/typeServices.ts
@@ -1,13 +1,18 @@
-import { Inputs } from "../types";
+import { InputsLogin, InputsRegister } from "../types";
 import { NavigateFunction } from "react-router-dom";
 
-export type onChangeProps = {
+export type onChangeLoginProps = {
   event: React.ChangeEvent<HTMLInputElement>;
-  setInputs: React.Dispatch<React.SetStateAction<Inputs | undefined>>;
+  setInputs: React.Dispatch<React.SetStateAction<InputsLogin | undefined>>;
 };
 
 export type submitProps = {
   event: React.FormEvent<HTMLFormElement>;
-  inputs: Inputs | undefined;
+  inputs: InputsLogin | undefined;
   navigate: NavigateFunction;
 };
+
+export type onChangeRegisterProps = {
+  event: React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLInputElement>
+  setInputs: React.Dispatch<React.SetStateAction<InputsRegister | undefined>>
+}
