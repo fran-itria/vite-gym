ab5405a653b8ee5f3e43676553acc594a452d60a
Franco Itria <francoitria01@gmail.com>
2024-01-17 17:59:07 -0300

    Added new type

--- src/types.tsx
+++ src/types.tsx
@@ -8,7 +8,20 @@ export type PropsNavHealth = {
   setInformation: React.Dispatch<React.SetStateAction<InformationEnum>>;
 };
 
-export type Inputs = {
+export type InputsLogin = {
   user?: string;
   password?: string;
+};
+
+export type InputsRegister = {
+  gymName: string,
+  name: string,
+  surname: string,
+  contacEmergency: number,
+  dni: number,
+  age: number,
+  email: string,
+  phone: number,
+  user: string;
+  password: string;
 };
