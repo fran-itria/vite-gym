2afccca428d30b873125f9b80c5c39ec81f92309
Franco Itria <francoitria01@gmail.com>
2024-01-17 23:08:00 -0300

    Added select props and labelName

--- src/components/Session/FormElement.tsx
+++ src/components/Session/FormElement.tsx
@@ -4,14 +4,17 @@ import { Elements } from "../../types";
 export default function FormElement({ labelName, type, name, setInputs, gyms }: Elements) {
     return (
         !gyms ?
-            <label >
+            <label>
                 {labelName}:
-                <input type={type} name={name} onChange={(event) => onChange({ event, setInputs })}></input>
+                <input type={type} name={name} onChange={(event) => onChange({ event, setInputs })} required={true}></input>
             </label>
             :
-            <select>
-                <option></option>
-                {gyms?.map(gym => <option value={gym.name}>{gym.name}</option>)}
-            </select>
+            <>
+                {labelName}
+                <select name={name} required={true} onChange={(event) => onChange({ event, setInputs })}>
+                    <option></option>
+                    {gyms?.map(gym => <option value={gym.name}>{gym.name}</option>)}
+                </select>
+            </>
     )
 }
