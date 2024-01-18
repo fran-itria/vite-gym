29818a0418b19a99a95c6169fca0a3f664466581
Franco Itria <francoitria01@gmail.com>
2024-01-17 21:14:36 -0300

    Added new conts

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,38 @@
 export const baseUrl = "http://localhost:3001";
 
 export const miSalud = "miSalud";
+
+export const typesElement = {
+    text: "text",
+    number: "number",
+    email: "email",
+    phone: "phone",
+    password: "password",
+    tel: "tel"
+}
+
+export const namesElements = {
+    name: "name",
+    email: "email",
+    gymName: "gymName",
+    age: "age",
+    surname: "surname",
+    dni: "dni",
+    contactEmergency: "contactEmergency",
+    user: "user",
+    password: "password",
+    phone: "phone"
+}
+
+export const labels = {
+    name: "Nombre",
+    email: "Email",
+    gymName: "Nombre del gimnasio",
+    age: "Edad",
+    surname: "Apellido",
+    dni: "Dni",
+    contactEmergency: "Contacto de emergencia",
+    user: "Usuario",
+    password: "Contraseña",
+    phone: "Telefono"
+}
