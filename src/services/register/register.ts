d25794b50648574b480b96b9639d705e32da2578
Franco Itria <francoitria01@gmail.com>
2024-01-19 02:30:46 -0300

    Delete base url

--- src/services/register/register.ts
+++ src/services/register/register.ts
@@ -6,8 +6,8 @@ import { baseUrl } from "../../const";
 export async function register({ inputs, url }: registerProps): Promise<AxiosResponse<any, any>> {
     if (url?.pathname.includes("admin")) {
         const { gymName }: any = inputs
-        await axios.post(`${baseUrl}/gym`, { name: gymName })
-        const response = await axios.post(`${baseUrl}/user`, { ...inputs, admin: true })
+        await axios.post(`/gym`, { name: gymName })
+        const response = await axios.post(`/user`, { ...inputs, admin: true })
         return response
     } else {
         const response = await axios.post(`${baseUrl}/user`, inputs)
