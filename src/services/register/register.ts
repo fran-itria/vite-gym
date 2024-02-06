1acbabc8803b208cccd0c2769600eb8d0d343223
Franco Itria <francoitria01@gmail.com>
2024-02-06 19:36:27 -0300

    Deleet baseUrl

--- src/services/register/register.ts
+++ src/services/register/register.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios, { AxiosResponse } from "axios";
 import { registerProps } from "../typeServices";
-import { baseUrl } from "../../const";
 
 export async function register({ inputs, url }: registerProps): Promise<AxiosResponse<any, any>> {
     if (url?.pathname.includes("admin")) {
@@ -10,7 +9,7 @@ export async function register({ inputs, url }: registerProps): Promise<AxiosRes
         const response = await axios.post(`/user`, { ...inputs, admin: true })
         return response
     } else {
-        const response = await axios.post(`${baseUrl}/user`, inputs)
+        const response = await axios.post(`/user`, inputs)
         return response
     }
 }
