9a18f7e285fd64ebdc8ba8463ef7f78cea8626a5
Franco Itria <francoitria01@gmail.com>
2024-02-07 19:13:41 -0300

    Added information user

--- src/components/Resume/Resume.tsx
+++ src/components/Resume/Resume.tsx
@@ -1,8 +1,17 @@
+import { useAppSelector } from "../../hook/store"
 
 export default function Resume() {
+    const user = useAppSelector(state => state.user)
     return (
         <div>
-            Resumen del usuario
+            <p> <b>Nombre: </b> {user.name}</p>
+            <p> <b>Mail: </b> {user.email}</p>
+            <p> <b>Edad: </b> {user.age}</p>
+            <p> <b>Dni: </b> {user.dni}</p>
+            <p> <b>Numero de telefono: </b> {user.phone}</p>
+            <p> <b>Contacto de emergencia: </b> {user.contactEmergency}</p>
+            <p> <b>Rutinas: </b> {user.Routines.length}</p>
+            <p> <b>Suscripción: </b> {user.pay ? '✅' : '❌'}</p>
         </div>
     )
 }
