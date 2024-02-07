ccafb5c79b51e8552734c4376eb089e9ed742849
Franco Itria <francoitria01@gmail.com>
2024-02-07 19:13:26 -0300

    Gym name header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,25 @@
 import { NavLink, useLocation } from "react-router-dom";
 import style from './Header.module.css'
 import { useAppSelector } from "../../hook/store";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Header() {
     const { pathname } = useLocation()
     const { id } = useAppSelector(state => state.user)
+    const [gymName, setGymName] = useState<string>('')
+    const { GymId } = useAppSelector(state => state.user)
+
+    useEffect(() => {
+        axios.get(`/gym/getGymId/${GymId}`).then(response => {
+            console.log(response)
+            setGymName(response.data)
+        })
+    }, [GymId])
     return (
         <>
             <header className={style.header}>
-                <h1 className={style.gymName}>Nombre del gym</h1>
+                <h1 className={style.gymName}>{gymName}</h1>
                 {/* <img alt="Foto" className={style.perfil} /> */}
                 <div className={style.perfil}>P</div>
             </header>
