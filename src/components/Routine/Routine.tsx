e7ee25467c46462d21a44cba150d66ed50904d41
Franco Itria <francoitria01@gmail.com>
2024-02-13 20:14:49 -0300

    Added icons

--- src/components/Routine/Routine.tsx
+++ src/components/Routine/Routine.tsx
@@ -3,6 +3,10 @@ import { useEffect } from "react"
 import { useAppSelector } from "../../hook/store"
 import axios from "axios"
 import { useRoutineActions } from "../../hook/useRoutineActions"
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../../themeIcons/modifiedExerciseColors";
 
 export default function Routine() {
     const { name, surname, Routines } = useAppSelector(state => state.user)
@@ -20,13 +24,14 @@ export default function Routine() {
     useEffect(() => {
         console.log(routine)
     }, [routine])
+
     return (
         <>
             <p>Rutina de {name} {surname}</p>
             {routine.Days ?
                 routine.Days.map((day, i) => {
                     return (
-                        <details>
+                        <details key={day.id}>
                             <summary>Día {i + 1}</summary>
                             <table>
                                 <thead>
@@ -40,12 +45,18 @@ export default function Routine() {
                                 <tbody>
                                     {day.Exercises.map(exercise => {
                                         return (
-                                            <tr>
+                                            <tr key={exercise.id}>
                                                 <td>{exercise.name}</td>
                                                 <td>{exercise.series}</td>
                                                 <td>{exercise.reps}</td>
                                                 <td>
                                                     {exercise.Loads.map(load => <p>{load.loads}</p>)}
+                                                </td>
+                                                <td>
+                                                    <ThemeProvider theme={theme}>
+                                                        <CreateIcon sx={{ color: theme.palette.pencil.main }} />
+                                                        <DeleteIcon sx={{ color: theme.palette.tashIcon.light }} />
+                                                    </ThemeProvider>
                                                 </td>
                                             </tr>
                                         )
