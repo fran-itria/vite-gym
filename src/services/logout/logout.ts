d6595d92b4e3a4641886999d11b8bc5f7d98a93f
Franco Itria <francoitria01@gmail.com>
2024-02-13 03:11:43 -0300

    Logout function

---
+++ src/services/logout/logout.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+import { NavigateFunction } from "react-router-dom"
+
+export const logout = async (id: string | null, navigate: NavigateFunction) => {
+    try {
+        const logoutUser = await axios.put('/user/logout', { id })
+        console.log(logoutUser)
+        if (logoutUser.status == 200) navigate('/')
+        return logoutUser
+    } catch (error) {
+        console.log(error)
+        window.alert(error)
+    }
+}
