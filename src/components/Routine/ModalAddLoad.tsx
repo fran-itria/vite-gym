08e92d009a33e88b3e19c8121ec789b04faeb6a4
Franco Itria <francoitria01@gmail.com>
2024-02-15 01:53:37 -0300

    Modal add load component

---
+++ src/components/Routine/ModalAddLoad.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { useAppSelector } from "../../hook/store"
+import { useRoutineActions } from "../../hook/useRoutineActions"
+import { modifiedLoads } from "../../services/exercises/modifiedExercise"
+import { ModalAddLoadComponentProps } from "../../types"
+
+export default function ModalAddLoad({ idExercise, setOpenLoad }: ModalAddLoadComponentProps) {
+    const [inputLoad, setInputLoad] = useState<string>('')
+    const { routineActual } = useRoutineActions()
+    const { Routines } = useAppSelector(state => state.user)
+    const routineId = Routines[0].id
+
+    return (
+        <div style={{ border: 'solid, black, 2px', position: 'absolute', top: '50%', right: '50%', background: 'white' }}>
+            <label>
+                Carga:
+                <input onChange={(e) => setInputLoad(e.target.value)}></input>
+            </label>
+            <button onClick={() => modifiedLoads({
+                exerciseId: idExercise,
+                load: inputLoad,
+                routineId,
+                routineActual
+            })}>
+                Agregar
+            </button>
+            <button onClick={() => setOpenLoad((openLoad) => !openLoad)}>
+                Cerrar
+            </button>
+        </div>
+    )
+}
