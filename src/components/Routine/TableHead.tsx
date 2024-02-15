d556f78b37c62dfe0fa1545fcc9e2eadd631c966
Franco Itria <francoitria01@gmail.com>
2024-02-15 05:11:51 -0300

    Correct map weeks and added head empty

--- src/components/Routine/TableHead.tsx
+++ src/components/Routine/TableHead.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export default function TableHead({ weeksLoads }: { weeksLoads: any }) {
+export default function TableHead({ weeks }: { weeks: number | null }) {
+    const totalWeeks = new Array(weeks).fill(0)
     return (
         <thead>
             <tr>
+                <th></th>
                 <th>Ejercicio</th>
                 <th>Series</th>
                 <th>Repeticiones</th>
-                {weeksLoads.map((_week: any, i: number) => (<th>Semana {i + 1}</th>))}
+                {totalWeeks.map((_week, i: number) => <th>Semana {i + 1}</th>)}
             </tr>
         </thead>
     )
