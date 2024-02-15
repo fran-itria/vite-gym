ac6e3d1c6299255c70e9618dd587793356be4712
Franco Itria <francoitria01@gmail.com>
2024-02-15 05:53:57 -0300

    Added buttons for eliminate exercise

--- src/components/Routine/TableRow.tsx
+++ src/components/Routine/TableRow.tsx
@@ -7,11 +7,18 @@ import theme from "../../themeIcons/modifiedExerciseColors";
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { TableRowComponentProps } from "../../types";
 import ModalAddLoad from "./ModalAddLoad";
+import { useAppSelector } from "../../hook/store";
+import deleteExercise from "../../services/routine/exercises/deleteExercise";
+import { useRoutineActions } from "../../hook/useRoutineActions";
 
 export default function TableRow({ exercise }: TableRowComponentProps) {
     const [open, setOpen] = useState<boolean>(false)
     const [openLoad, setOpenLoad] = useState<boolean>(false)
     const [idExercise, setIdExercise] = useState<string | null>('')
+    const [confirmDelete, setConfirmDelete] = useState<boolean>(false)
+    const { Routines } = useAppSelector(state => state.user)
+    const routineId = Routines[0].id
+    const { routineActual } = useRoutineActions()
 
 
     return (
@@ -19,7 +26,10 @@ export default function TableRow({ exercise }: TableRowComponentProps) {
             <td>
                 <ThemeProvider theme={theme}>
                     <CreateIcon onClick={() => setOpen(!open)} sx={{ color: theme.palette.pencil.main }} />
-                    <DeleteIcon sx={{ color: theme.palette.tashIcon.light }} />
+                    <DeleteIcon sx={{ color: theme.palette.tashIcon.light }} onClick={() => {
+                        setIdExercise(exercise.id)
+                        setConfirmDelete(!confirmDelete)
+                    }} />
                 </ThemeProvider>
             </td>
             <td>{exercise.name}</td>
@@ -45,6 +55,20 @@ export default function TableRow({ exercise }: TableRowComponentProps) {
                 :
                 <></>
             }
-        </tr>
+            {confirmDelete ?
+                <div style={{ display: 'flex', flexDirection: 'column', background: 'white', border: '5px, solid, black', position: 'absolute', top: '50%', right: '50%' }}>
+                    <p style={{ color: 'black' }}>{idExercise}</p>
+                    <p> Desea eliminar el ejercicio {exercise.name} </p>
+                    <button style={{ background: 'red', color: 'white' }} onClick={() => deleteExercise(idExercise, routineId, routineActual, setConfirmDelete)}>
+                        Borrar
+                    </button>
+                    <button style={{ background: 'green', color: 'white' }} onClick={() => setConfirmDelete(!confirmDelete)}>
+                        Cancelar
+                    </button>
+                </div>
+                :
+                <></>
+            }
+        </tr >
     )
 }
