ee25a127c393caa2aa3d84b31c946eb380b67eb5
Franco Itria <francoitria01@gmail.com>
2024-02-16 03:09:52 -0300

    Use component create exercise

--- src/components/Routine/Routine.tsx
+++ src/components/Routine/Routine.tsx
@@ -2,8 +2,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import Table from "./Table";
 import { addWeek, deleteWeek } from "../../services/routine/modifiedWeeks";
-import createExerciseInputs from "../../services/routine/exercises/formCreate/craeteExerciseInputs";
-import addExerciseFunction from "../../services/routine/exercises/addExercise";
 import FormOneDay from "./CraeteOneDay/FormOneDay";
 import TableConfirmDay from "./CraeteOneDay/TableConfirmDay";
 import useDayCreate from "../../hook/Components/Routine/useCreateDay";
@@ -14,10 +12,11 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { ThemeProvider } from "@mui/material";
 import theme from "../../themeIcons/modifiedExerciseColors";
 import deleteDay from "../../services/routine/deleteDay";
+import CreateExercise from "./CraeteExercise/CreateExercise";
 
 export default function Routine() {
     const { name, routine, routineActual, routineId, surname } = useInformation()
-    const { addExercise, setAddExercise, inputs, setInputs } = useCreaetExercise()
+    const { addExercise, setAddExercise } = useCreaetExercise()
     const { addDay, dayCreate, pag, setAddDay, setDayCreate, setPag, setTotalExercise, totalExercise } = useDayCreate()
     useGetRoutine()
 
@@ -37,25 +36,7 @@ export default function Routine() {
                                 <button onClick={() => addWeek(routineId, routine.weeks + 1, routineActual)}>+ Semana</button>
                                 <button onClick={() => deleteWeek(routineId, routine.weeks - 1, routineActual)}>- Semana</button>
                                 {addExercise ?
-                                    <form
-                                        style={{ border: 'solid, red, 5px', borderRadius: '50px', display: 'flex', flexDirection: 'column', position: 'absolute' }}
-                                        onSubmit={(e) => addExerciseFunction({ e, dayId: day.id, exercise: day.Exercises.length + 1, inputs, routineId, setAddExercise, routineActual })}
-                                    >
-                                        <label>
-                                            Nombre del ejercicio:
-                                            <input name="exerciseName" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
-                                        </label>
-                                        <label>
-                                            Series:
-                                            <input name="series" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
-                                        </label>
-                                        <label>
-                                            Repeticiones:
-                                            <input name="reps" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
-                                        </label>
-                                        <button>Crear</button>
-                                        <button onClick={() => setAddExercise(!addExercise)}>Cancelar</button>
-                                    </form>
+                                    <CreateExercise day={day} setAddExercise={setAddExercise} />
                                     :
                                     <></>
                                 }
