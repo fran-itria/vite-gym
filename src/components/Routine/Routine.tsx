038dc010d397f625da316c5734636dbd7b81e4b6
Franco Itria <francoitria01@gmail.com>
2024-02-16 07:38:12 -0300

    Case in which there is no routine

--- src/components/Routine/Routine.tsx
+++ src/components/Routine/Routine.tsx
@@ -2,28 +2,30 @@ import FormOneDay from "./CraeteOneDay/FormOneDay";
 import TableConfirmDay from "./CraeteOneDay/TableConfirmDay";
 import useDayCreate from "../../hook/Components/Routine/useCreateDay";
 import useInformation from "../../hook/Components/Routine/useInformation";
-import useGetRoutine from "../../hook/Components/Routine/useGetRoutine";
 import Detail from "./Detail";
 import FormTotalExercise from "./FormTotalExercise";
 
 export default function Routine() {
-    const { name, routine, routineId, surname } = useInformation()
+    const { name, routine, surname, routineId } = useInformation()
     const { addDay, dayCreate, pag, setAddDay, setDayCreate, setPag, setTotalExercise, totalExercise } = useDayCreate()
-    useGetRoutine()
 
     return (
         <>
             <p>Rutina de {name} {surname}</p>
             {routine.Days?.length && routine.Days?.length > 0 ?
-                routine.Days.map((day, i) => {
-                    return (
-                        <Detail day={day} i={i} />
-                    )
-                })
+                <>
+                    {routine.Days.map((day, i) => {
+                        return (
+                            <Detail day={day} i={i} />
+                        )
+                    })}
+                    <button onClick={() => setAddDay(!addDay)}>+ Día</button>
+                </>
                 :
-                <p>No tienes rutina actualmente</p>
+                <>
+                    <p>No tienes rutina actualmente</p>
+                </>
             }
-            <button onClick={() => setAddDay(!addDay)}>+ Día</button>
             {addDay ?
                 <FormTotalExercise setPag={setPag} setTotalExercise={setTotalExercise} setAddDay={setAddDay} />
                 :
