f81d3410993bed366d7224680a7a2aa0207ee6a8
Franco Itria <francoitria01@gmail.com>
2024-02-16 02:32:42 -0300

    Delete day function

---
+++ src/services/routine/deleteDay.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import axios from "axios"
+
+export default async function deleteDay(id: string | undefined, routineId: string, routineActual: Function) {
+    try {
+        const response = await axios.delete(`/day/delete/${id}`)
+        const routine = await axios.get(`/rutina/${routineId}`)
+        if (response.status == 200) window.alert(response.data.Message)
+        routineActual(routine.data)
+    } catch (error) {
+        console.log(error)
+        window.alert(error)
+    }
+}
