ccc552e9fb78a9887e0dad0d77fe6584ef2fd9b2
Franco Itria <francoitria01@gmail.com>
2024-02-16 05:37:15 -0300

    Remove deleteWeek function

--- src/services/routine/modifiedWeeks.ts
+++ src/services/routine/modifiedWeeks.ts
@@ -5,10 +5,3 @@ export async function addWeek(id: string, weeks: number, routineActual: Function
     const actualRoutine = await axios.put('/rutina', { id, weeks })
     routineActual(actualRoutine.data)
 }
-
-export async function deleteWeek(id: string, weeks: number, routineActual: Function) {
-    if (weeks > 0) {
-        const actualRoutine = await axios.put('/rutina', { id, weeks })
-        routineActual(actualRoutine.data)
-    } else window.alert('No se puede tener menos de 1 semana')
-}
