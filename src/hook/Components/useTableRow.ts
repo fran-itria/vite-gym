c4aa4db7e344b3d896dc5eff206fe0a5eafff99b
Franco Itria <francoitria01@gmail.com>
2024-02-16 05:25:19 -0300

    Delete state idExercise

--- src/hook/Components/useTableRow.ts
+++ src/hook/Components/useTableRow.ts
@@ -3,8 +3,7 @@ import { useState } from "react"
 export default function useTabelRow() {
     const [open, setOpen] = useState<boolean>(false)
     const [openLoad, setOpenLoad] = useState<boolean>(false)
-    const [idExercise, setIdExercise] = useState<string | undefined>('')
     const [confirmDelete, setConfirmDelete] = useState<boolean>(false)
 
-    return { open, setOpen, openLoad, setOpenLoad, idExercise, setIdExercise, confirmDelete, setConfirmDelete }
+    return { open, setOpen, openLoad, setOpenLoad, confirmDelete, setConfirmDelete }
 }
