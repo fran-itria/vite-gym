f0f643d80b26ce559b92755733a1a8d5be329833
Franco Itria <francoitria01@gmail.com>
2024-02-16 05:26:02 -0300

    Delete setIdExercise

--- src/components/Routine/TableRow/TableCell.tsx
+++ src/components/Routine/TableRow/TableCell.tsx
@@ -6,15 +6,14 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { TableCellComponentProps } from '../../../types';
 import useTabelRow from '../../../hook/Components/useTableRow';
 
-export default function TableCell({ Loads, name, series, reps, id, weeks, setOpenLoad, setIdExercise }: TableCellComponentProps) {
-    const { setOpen, setConfirmDelete } = useTabelRow()
+export default function TableCell({ Loads, name, series, reps, weeks, setOpenLoad, setConfirmDelete }: TableCellComponentProps) {
+    const { setOpen } = useTabelRow()
     return (
         <>
             <td>
                 <ThemeProvider theme={theme}>
                     <CreateIcon onClick={() => setOpen(open => !open)} sx={{ color: theme.palette.pencil.main }} />
                     <DeleteIcon sx={{ color: theme.palette.tashIcon.light }} onClick={() => {
-                        setIdExercise(id)
                         setConfirmDelete(confirmDelete => !confirmDelete)
                     }} />
                 </ThemeProvider>
@@ -36,7 +35,6 @@ export default function TableCell({ Loads, name, series, reps, id, weeks, setOpe
                     <td>
                         <AddCircleIcon color="success" onClick={() => {
                             setOpenLoad(openLoad => !openLoad)
-                            setIdExercise(id)
                         }} />
                     </td>
                     :
