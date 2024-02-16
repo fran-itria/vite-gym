43c46acb4a8a780bf728d6ceab0c054d87c13d0e
Franco Itria <francoitria01@gmail.com>
2024-02-16 03:09:30 -0300

    Create exercise form component

---
+++ src/components/Routine/CraeteExercise/CreateExercise.tsx
@@ -0,0 +1,31 @@
+import useCreaetExercise from "../../../hook/Components/Routine/useCreateExercise";
+import useInformation from "../../../hook/Components/Routine/useInformation";
+import addExerciseFunction from "../../../services/routine/exercises/addExercise";
+import createExerciseInputs from "../../../services/routine/exercises/formCreate/craeteExerciseInputs";
+import { CreateExerciseComponentProps } from "../../../types";
+
+export default function CreateExercise({ setAddExercise, day }: CreateExerciseComponentProps) {
+    const { inputs, setInputs, addExercise } = useCreaetExercise()
+    const { routineActual, routineId } = useInformation()
+    return (
+        <form
+            style={{ border: 'solid, red, 5px', borderRadius: '50px', display: 'flex', flexDirection: 'column', position: 'absolute' }}
+            onSubmit={(e) => addExerciseFunction({ e, dayId: day.id, exercise: day.Exercises.length + 1, inputs, routineId, setAddExercise, routineActual })}
+        >
+            <label>
+                Nombre del ejercicio:
+                <input name="exerciseName" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
+            </label>
+            <label>
+                Series:
+                <input name="series" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
+            </label>
+            <label>
+                Repeticiones:
+                <input name="reps" onChange={(e) => createExerciseInputs({ e, setInputs })}></input>
+            </label>
+            <button>Crear</button>
+            <button onClick={() => setAddExercise(!addExercise)}>Cancelar</button>
+        </form>
+    )
+}
