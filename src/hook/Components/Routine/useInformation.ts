3a284c23324429164f0d37b33d988cbe38b439b0
Franco Itria <francoitria01@gmail.com>
2024-02-16 07:39:05 -0300

    use useEffect for call routine

--- src/hook/Components/Routine/useInformation.ts
+++ src/hook/Components/Routine/useInformation.ts
@@ -1,11 +1,28 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react"
 import { useAppSelector } from "../../store"
 import { useRoutineActions } from "../../useRoutineActions"
+import axios from "axios"
 
 const useInformation = () => {
     const { name, surname, Routines } = useAppSelector(state => state.user)
     const routine = useAppSelector(state => state.routine)
     const { routineActual } = useRoutineActions()
-    const routineId = Routines[0].id
+    let routineId
+    if (Routines.length > 0) {
+        routineId = Routines[Routines.length - 1].id
+    }
+
+    useEffect(() => {
+        if (Routines.length > 0) {
+            const routineId = Routines[0].id
+            axios.get(`/rutina/${routineId}`)
+                .then(response => {
+                    routineActual(response.data)
+                })
+                .catch(error => console.log(error))
+        }
+    }, [])
 
     return { name, surname, routine, routineActual, routineId }
 }
