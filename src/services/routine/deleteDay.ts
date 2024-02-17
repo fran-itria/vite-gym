1dfdba9408db3851e8cc4ffdaf4c1e1f504f30c7
Franco Itria <francoitria01@gmail.com>
2024-02-17 08:08:59 -0300

    routineId can be Undefined

--- src/services/routine/deleteDay.ts
+++ src/services/routine/deleteDay.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios"
 
-export default async function deleteDay(id: string | undefined, routineId: string, routineActual: Function) {
+export default async function deleteDay(id: string | undefined, routineId: string | undefined, routineActual: Function) {
     try {
         const response = await axios.delete(`/day/delete/${id}`)
         const routine = await axios.get(`/rutina/${routineId}`)
--- src/services/routine/modifiedWeeks.ts
+++ src/services/routine/modifiedWeeks.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios"
 
-export async function addWeek(id: string, weeks: number, routineActual: Function) {
+export async function addWeek(id: string | undefined, weeks: number, routineActual: Function) {
     const actualRoutine = await axios.put('/rutina', { id, weeks })
     routineActual(actualRoutine.data)
 }
