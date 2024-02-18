0b4fe952ad704ef21e70302a0b48ec7cab5a5a1b
Franco Itria <francoitria01@gmail.com>
2024-02-18 15:59:03 -0300

    Use pagDays for day number

--- src/services/routine/addDayRoutine.ts
+++ src/services/routine/addDayRoutine.ts
@@ -1,7 +1,7 @@
 import { addDayRoutineProps } from "../typeServices"
 
-export default function addDayRoutine({ setRoutine, setAddDay, setDayCreate, setPag, setTotalExercise, dayCreate, setPagDays }: addDayRoutineProps) {
-    setRoutine(prev => [...prev, { exercises: dayCreate }])
+export default function addDayRoutine({ setRoutine, setAddDay, setDayCreate, setPag, setTotalExercise, dayCreate, setPagDays, pagDays }: addDayRoutineProps) {
+    setRoutine(prev => [...prev, { day: pagDays, exercises: dayCreate }])
     setAddDay(true)
     setDayCreate([])
     setPag(0)
