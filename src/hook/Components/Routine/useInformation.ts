ff1c3f679e5fe9d4021503f80d7dec26732218fc
Franco Itria <francoitria01@gmail.com>
2024-02-17 23:52:45 -0300

    Created state for routineId

--- src/hook/Components/Routine/useInformation.ts
+++ src/hook/Components/Routine/useInformation.ts
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useAppSelector } from "../../store"
 import { useRoutineActions } from "../../useRoutineActions"
 import axios from "axios"
@@ -8,17 +8,17 @@ const useInformation = () => {
     const { name, surname, Routines } = useAppSelector(state => state.user)
     const routine = useAppSelector(state => state.routine)
     const { routineActual } = useRoutineActions()
-    let routineId
+    const [routineId, setRoutineId] = useState<string | undefined>(undefined)
     useEffect(() => {
         if (Routines.length > 0) {
-            routineId = Routines[Routines.length - 1].id
+            setRoutineId(Routines[Routines.length - 1].id)
             axios.get(`/rutina/${routineId}`)
                 .then(response => {
                     routineActual(response.data)
                 })
                 .catch(error => console.log(error))
         }
-    }, [])
+    }, [routineId])
 
     return { name, surname, routine, routineActual, routineId }
 }
