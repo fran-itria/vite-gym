45640b64a2d52c2c4dae7edf61cbed1110f6a5e7
Franco Itria <francoitria01@gmail.com>
2024-02-18 01:13:00 -0300

    Use types

--- src/services/routine/confirmRoutine.ts
+++ src/services/routine/confirmRoutine.ts
@@ -1,21 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios"
 import createRoutine from "./craeteRoutine"
-import { createRoutineProps } from "../typeServices"
-import { Routine } from "../../store/routine/slice"
-import { RoutinesUser } from "../../store/user/slice"
+import { confirmRoutineProps } from "../typeServices"
 
-export default async function confirmRoutine({ actualiceRoutinesUser, days, routineActual, userId, setOpenCreateRouitine }: createRoutineProps & {
-    routineActual: (Days: Routine) => void
-    actualiceRoutinesUser: (routine: RoutinesUser) => void
-    setOpenCreateRouitine: React.Dispatch<React.SetStateAction<boolean>>
-}) {
+export default async function confirmRoutine({ updateRoutinesUser, days, routineActual, userId, setOpenCreateRouitine }: confirmRoutineProps) {
     try {
         const response = await createRoutine({ days, userId })
         const rutina = await axios.get(`/rutina/${response?.routine.id}`)
         window.alert('Rutina creada exitosamente')
         routineActual(rutina.data)
-        actualiceRoutinesUser(response?.user)
+        updateRoutinesUser(response?.user)
         setOpenCreateRouitine(prevState => !prevState)
     } catch (error: any) {
         console.log(error)
--- src/services/routine/deleteRoutine.ts
+++ src/services/routine/deleteRoutine.ts
@@ -1,19 +1,13 @@
 import axios from "axios";
-import { Routine } from "../../store/routine/slice";
-import { RoutinesUser } from "../../store/user/slice";
+import { deleteRoutineProps } from "../typeServices";
 
-export default async function deletRoutine({ id, routineActual, userId, actualiceRoutinesUser }: {
-    id: string | undefined,
-    routineActual: (Days: Routine) => void
-    actualiceRoutinesUser: (routine: RoutinesUser) => void
-    userId: string | null
-}) {
+export default async function deletRoutine({ id, routineActual, userId, updateRoutinesUser }: deleteRoutineProps) {
     try {
         const response = await axios.delete(`/rutina/delete/${id}`)
         const user = await axios.get(`/user/getOneUser/${userId}`)
         if (response.status == 200) window.alert(response.data.Message)
         routineActual({ weeks: 0, Days: undefined })
-        actualiceRoutinesUser(user.data)
+        updateRoutinesUser(user.data)
     } catch (error) {
         console.log(error)
         window.alert(error)
