e61283db8b1abd30a0e13683efa29b65b49de702
Franco Itria <francoitria01@gmail.com>
2024-02-21 08:51:33 -0300

    Use loader

--- src/components/Session/register/FormRegister.tsx
+++ src/components/Session/register/FormRegister.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
 import { InputsRegister } from "../../../types";
-import { labels, namesElements, typesElement } from "../../../const";
+import { labels, loaders, namesElements, typesElement } from "../../../const";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import onSubmit from "../../../services/form/onSubmit";
 import FormElement from "../FormElement";
 import { getId } from "../../../services/getId";
 import { useUserActions } from "../../../hook/useUserActions";
+import useLoaders from "../../../hook/Components/useLoaders";
+import Loader from "../../Loader";
 
 export default function FormRegister() {
     const [inputs, setInputs] = useState<InputsRegister>()
@@ -14,6 +16,7 @@ export default function FormRegister() {
     const params = useParams()
     const navigate = useNavigate()
     const { addUser } = useUserActions()
+    const { pending, setPending } = useLoaders()
 
     useEffect(() => {
         const { id } = params
@@ -25,22 +28,25 @@ export default function FormRegister() {
     }, [])
 
     return (
-        <form onSubmit={(event) => onSubmit({ event, inputs, navigate, addUser, url })}>
-            {url.pathname.includes("admin") ?
-                <FormElement labelName={labels.gymName} type={typesElement.text} name={namesElements.gymName} setInputs={setInputs}></FormElement>
-                :
-                <></>
-            }
-            <FormElement labelName={labels.age} type={typesElement.number} name={namesElements.age} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.name} type={typesElement.text} name={namesElements.name} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.surname} type={typesElement.text} name={namesElements.surname} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.dni} type={typesElement.number} name={namesElements.dni} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.email} type={typesElement.email} name={namesElements.email} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.phone} type={typesElement.tel} name={namesElements.phone} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.contactEmergency} type={typesElement.tel} name={namesElements.contactEmergency} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.user} type={typesElement.text} name={namesElements.user} setInputs={setInputs}></FormElement>
-            <FormElement labelName={labels.password} type={typesElement.password} name={namesElements.password} setInputs={setInputs}></FormElement>
-            <button>Registrarme</button>
-        </form>
+        !pending ?
+            <form onSubmit={(event) => onSubmit({ event, inputs, navigate, addUser, url, setPending })}>
+                {url.pathname.includes("admin") ?
+                    <FormElement labelName={labels.gymName} type={typesElement.text} name={namesElements.gymName} setInputs={setInputs}></FormElement>
+                    :
+                    <></>
+                }
+                <FormElement labelName={labels.age} type={typesElement.number} name={namesElements.age} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.name} type={typesElement.text} name={namesElements.name} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.surname} type={typesElement.text} name={namesElements.surname} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.dni} type={typesElement.number} name={namesElements.dni} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.email} type={typesElement.email} name={namesElements.email} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.phone} type={typesElement.tel} name={namesElements.phone} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.contactEmergency} type={typesElement.tel} name={namesElements.contactEmergency} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.user} type={typesElement.text} name={namesElements.user} setInputs={setInputs}></FormElement>
+                <FormElement labelName={labels.password} type={typesElement.password} name={namesElements.password} setInputs={setInputs}></FormElement>
+                <button>Registrarme</button>
+            </form>
+            :
+            <Loader text={loaders.init} />
     )
 }
