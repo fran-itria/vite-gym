beef34e02efcc83d6ca181ed38f16005d4a1da28
Franco Itria <francoitria01@gmail.com>
2024-02-24 03:11:10 -0300

    Delete day and delete routine

--- src/services/routine/deleteDay.ts
+++ src/services/routine/deleteDay.ts
@@ -1,12 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios"
+import { deleteDayProps } from "../typeServices"
 
-export default async function deleteDay(id: string | undefined, routineId: string | undefined, routineActual: Function) {
+export default async function deleteDay({ id, routineId, routineActual, warmUpId, warmUpActual }: deleteDayProps) {
     try {
         const response = await axios.delete(`/day/delete/${id}`)
-        const routine = await axios.get(`/rutina/${routineId}`)
         if (response.status == 200) window.alert(response.data.Message)
-        routineActual(routine.data)
+        if (routineId && routineActual) {
+            const routine = await axios.get(`/rutina/${routineId}`)
+            routineActual(routine.data)
+        } else if (warmUpId && warmUpActual) {
+            const warmUp = await axios.get(`/calentamiento/${warmUpId}`)
+            warmUpActual(warmUp.data)
+        }
     } catch (error) {
         console.log(error)
         window.alert(error)
--- src/services/routine/deleteRoutine.ts
+++ src/services/routine/deleteRoutine.ts
@@ -1,13 +1,26 @@
 import axios from "axios";
-import { deleteRoutineProps } from "../typeServices";
+import { deleteRoutineProps, deleteWarmUpProps } from "../typeServices";
 
-export default async function deletRoutine({ id, routineActual, userId, updateRoutinesUser }: deleteRoutineProps) {
+export async function deletRoutine({ id, routineActual, userId, updateRoutinesUser }: deleteRoutineProps) {
     try {
         const response = await axios.delete(`/rutina/delete/${id}`)
         const user = await axios.get(`/user/getOneUser/${userId}`)
         if (response.status == 200) window.alert(response.data.Message)
         routineActual({ weeks: 0, Days: undefined })
         updateRoutinesUser(user.data)
+    } catch (error) {
+        console.log(error)
+        window.alert(error)
+    }
+}
+
+export async function deleteWarmup({ id, warmUpActual, userId, updateWarmUpUser }: deleteWarmUpProps) {
+    try {
+        const response = await axios.delete(`/calentamiento/delete/${id}`)
+        const user = await axios.get(`/user/getOneUser/${userId}`)
+        if (response.status == 200) window.alert(response.data.Message)
+        warmUpActual({ Days: undefined })
+        updateWarmUpUser(user.data)
     } catch (error) {
         console.log(error)
         window.alert(error)
