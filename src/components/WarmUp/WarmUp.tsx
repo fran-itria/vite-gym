90580ec54274277d3ddedc917c4745b7ac7aebee
Franco Itria <francoitria01@gmail.com>
2024-02-24 06:53:45 -0300

    Use component detail

--- src/components/WarmUp/WarmUp.tsx
+++ src/components/WarmUp/WarmUp.tsx
@@ -1,6 +1,6 @@
 import useDayCreate from "../../hook/Components/Routine/useCreateDay";
 import { deleteWarmup } from "../../services/routine/deleteRoutine";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useUserActions } from "../../hook/useUserActions";
 import Loader from "../Loader";
 import { loaders } from "../../const";
@@ -9,22 +9,15 @@ import FormTotalExercise from "../Routine/FormTotalExercise";
 import FormOneDay from "../Routine/CraeteOneDay/FormOneDay";
 import TableConfirmDay from "../Routine/CraeteOneDay/TableConfirmDay";
 import CreateRoutine from "../Routine/CreateRoutine";
-import useCreaetExercise from "../../hook/Components/Routine/useCreateExercise";
-import CreateExercise from "../Routine/CraeteExercise/CreateExercise";
-import DeleteIcon from '@mui/icons-material/Delete';
-import { ThemeProvider } from "@mui/material";
-import theme from "../../themeIcons/customTheme";
-import deleteDay from "../../services/routine/deleteDay";
+import Chronometer from "../Chronometer";
+import Detail from "../Routine/Detail";
 
 export default function WarmUp() {
     const { WarmUps, id, pending, setPending, updateWarmUpIdGlobal, warmUpActual, warmUpId, warmUp, Gym } = useWarmUpRoutine()
     const { addDay, dayCreate, pag, setAddDay, setDayCreate, setPag, setTotalExercise, totalExercise } = useDayCreate()
     const [createWarm, setCreateWarm] = useState<boolean>(false)
     const { updateWarmUpUser } = useUserActions()
-    const { addExercise, setAddExercise } = useCreaetExercise()
 
-    useEffect(() => console.log(warmUp), [warmUp])
-    useEffect(() => console.log(WarmUps), [WarmUps])
     return (
         <>
             {pending ? <Loader text={loaders.routine} />
@@ -50,52 +43,14 @@ export default function WarmUp() {
                             {warmUp.Days.map((day, i) => {
                                 return (
                                     <>
-                                        <details key={day.id}>
-                                            <summary>
-                                                Día {i + 1}
-                                            </summary>
-                                            <thead>
-                                                <tr>
-                                                    <th>Ejercicio</th>
-                                                    <th>Series</th>
-                                                    <th>Repeticiones</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {day.Exercises.map((exercise) => {
-                                                    return (
-                                                        <tr>
-                                                            <td>{exercise.name}</td>
-                                                            <td>{exercise.series}</td>
-                                                            <td> {exercise.reps}</td>
-                                                        </tr>
-                                                    )
-                                                })}
-                                            </tbody>
-                                            <button onClick={() => setAddExercise(!addExercise)}> + Ejercicio</button>
-                                            {addExercise ?
-                                                <CreateExercise
-                                                    day={day}
-                                                    setAddExercise={setAddExercise}
-                                                    warmUpId={warmUpId}
-                                                    warmUpActual={warmUpActual}
-                                                />
-                                                :
-                                                <></>
-                                            }
-                                        </details >
-                                        <ThemeProvider theme={theme}>
-                                            <DeleteIcon
-                                                sx={{ color: theme.palette.tashIcon.light }}
-                                                onClick={() => deleteDay({ id: day.id, warmUpId: warmUpId.id, warmUpActual })}
-                                            />
-                                        </ThemeProvider>
+                                        <Detail day={day} i={i} routineOrWarmUp={{ warmUpId: warmUpId.id, warmUpActual }} />
                                     </>
                                 )
                             })}
                             <button onClick={() => setAddDay(!addDay)}>+ Día</button>
-                            <button onClick={() => deleteWarmup({ id: warmUpId.id, warmUpActual, userId: id, updateWarmUpUser })}>Borrar rutina</button>
+                            <button onClick={() => deleteWarmup({ id: warmUpId.id, warmUpActual, userId: id, updateWarmUpUser })}>Borrar calentamiento</button>
                             <button onClick={() => setCreateWarm(prev => !prev)}> + Calentamiento </button>
+                            <Chronometer />
                         </>
                         :
                         <>
@@ -136,6 +91,7 @@ export default function WarmUp() {
                         updateWarmUpUser={updateWarmUpUser}
                         createWarm={createWarm}
                         gymName={Gym?.name}
+                        updateWarmUpIdGlobal={updateWarmUpIdGlobal}
                     />
                     :
                     <></>
