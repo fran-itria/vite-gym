0ec188b5738d6c2dd15a573ac5fc366ec06eb01e
Franco Itria <francoitria01@gmail.com>
2024-02-24 00:45:43 -0300

    eslint-disable react-hooks/exhaustive-deps

--- src/components/Admin/Users/Users.tsx
+++ src/components/Admin/Users/Users.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
 import { UsersComponent } from "../../../types"
 import Table from '@mui/material/Table';
