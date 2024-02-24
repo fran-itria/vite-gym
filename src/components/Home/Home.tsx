3d25c99cdecc902dd53788662fa61ce87c5a6422
Franco Itria <francoitria01@gmail.com>
2024-02-23 23:46:00 -0300

    If you are an administrator

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,17 +5,22 @@ import Resume from "../Resume/Resume";
 import Shifts from "../Shifts/Shifts";
 import { selects } from "../../const";
 import NotFound from "../NotFound";
+import { useAppSelector } from "../../hook/store";
 
 export default function Home() {
   const { userId, select } = useParams()
+  const { admin } = useAppSelector(state => state.user)
 
   return (
     <>
       {userId != 'null' ?
-        <>
-          <NavHome />
-          {select == selects.summary ? <Resume /> : select == selects.miSalud ? <Health /> : < Shifts />}
-        </>
+        !admin ?
+          <>
+            <NavHome />
+            {select == selects.summary ? <Resume /> : select == selects.miSalud ? <Health /> : < Shifts />}
+          </>
+          :
+          <></>
         :
         <NotFound />
       }
