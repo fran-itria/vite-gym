d2acd659e91fc7ea4c4e50076dec45b614274848
Franco Itria <francoitria01@gmail.com>
2024-02-24 06:43:43 -0300

    Delete exercise

--- src/services/routine/exercises/deleteExercise.ts
+++ src/services/routine/exercises/deleteExercise.ts
@@ -1,14 +1,28 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import axios from "axios";
+import { deleteExerciseProps } from "../../typeServices";
 
-export default async function deleteExercise(idExercise: string | undefined, routineId: string | undefined, routineActual: Function, setConfirmDelete: React.Dispatch<React.SetStateAction<boolean>>
-) {
+export default async function deleteExercise({
+    idExercise,
+    setConfirmDelete,
+    routineActual,
+    routineId,
+    warmUpActual,
+    warmUpId
+}: deleteExerciseProps) {
     try {
         const response = await axios.delete(`/ejercicio/delete/${idExercise}`)
-        const routine = await axios.get(`/rutina/${routineId}`)
-        if (response.status == 200) window.alert(response.data.Message)
-        setConfirmDelete(confirmDelete => !confirmDelete)
-        routineActual(routine.data)
+        if (routineId && routineActual) {
+            const routine = await axios.get(`/rutina/${routineId}`)
+            if (response.status == 200) window.alert(response.data.Message)
+            setConfirmDelete(confirmDelete => !confirmDelete)
+            routineActual(routine.data)
+        } else if (warmUpId && warmUpActual) {
+            const routine = await axios.get(`/calentamiento/${warmUpId}`)
+            if (response.status == 200) window.alert(response.data.Message)
+            setConfirmDelete(confirmDelete => !confirmDelete)
+            warmUpActual(routine.data)
+        }
     } catch (error) {
         console.log(error)
         window.alert(error)
