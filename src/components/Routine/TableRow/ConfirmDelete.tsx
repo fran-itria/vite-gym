789c3c9ba60ccfb235a9e0ed6b9f2ad8c3ddcbfd
Franco Itria <francoitria01@gmail.com>
2024-02-24 06:52:11 -0300

    Passed correct properties to delete exercise function

--- src/components/Routine/TableRow/ConfirmDelete.tsx
+++ src/components/Routine/TableRow/ConfirmDelete.tsx
@@ -1,18 +1,22 @@
-import useInformation from "../../../hook/Components/Routine/useInformation"
-import { useRoutineActions } from "../../../hook/useRoutineActions"
 import deleteExercise from "../../../services/routine/exercises/deleteExercise"
+import { ConfirmDeleteComponentProps } from "../../../types"
 
-export default function ConfirmDelete({ name, id, setConfirmDelete }: {
-    name: string | undefined,
-    id: string | undefined,
-    setConfirmDelete: React.Dispatch<React.SetStateAction<boolean>>
-}) {
-    const { routineId } = useInformation()
-    const { routineActual } = useRoutineActions()
+export default function ConfirmDelete({
+    name,
+    id,
+    setConfirmDelete,
+    routineActual,
+    routineId,
+    warmUpActual,
+    warmUpId
+}: ConfirmDeleteComponentProps) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', background: 'white', border: '5px, solid, black', position: 'absolute', top: '50%', right: '50%' }}>
             <p> Desea eliminar el ejercicio {name} </p>
-            <button style={{ background: 'red', color: 'white' }} onClick={() => deleteExercise(id, routineId.id, routineActual, setConfirmDelete)}>
+            <button style={{ background: 'red', color: 'white' }} onClick={() => {
+                if (id && routineId && routineActual) deleteExercise({ idExercise: id, routineId, routineActual, setConfirmDelete })
+                else if (id && warmUpId && warmUpActual) deleteExercise({ idExercise: id, warmUpId, warmUpActual, setConfirmDelete })
+            }}>
                 Borrar
             </button>
             <button style={{ background: 'green', color: 'white' }} onClick={() => setConfirmDelete(confirmDelete => !confirmDelete)}>
