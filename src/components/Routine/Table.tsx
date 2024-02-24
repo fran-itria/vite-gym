1a3aed8c90613f032ee413b2ba93dd136f9b50ce
Franco Itria <francoitria01@gmail.com>
2024-02-24 06:51:05 -0300

    Ues new properties

--- src/components/Routine/Table.tsx
+++ src/components/Routine/Table.tsx
@@ -4,26 +4,32 @@ import { Exercise, TableComponentProps } from '../../types';
 import TableHead from './TableHead';
 import TableRow from './TableRow/TableRow';
 
-export default function Table({ day, weeks }: TableComponentProps) {
+export default function Table({ day, weeks, routineActual, routineId, warmUpActual, warmUpId }: TableComponentProps) {
     return (
         <table>
-            <TableHead weeks={weeks} />
-            <tbody>
-                {day.Exercises.map((exercise: Exercise) => {
-                    return (
-                        <TableRow
-                            key={exercise.id}
-                            id={exercise.id}
-                            name={exercise.name}
-                            series={exercise.series}
-                            reps={exercise.reps}
-                            Loads={exercise.Loads}
-                            weeks={weeks}
-                        />
-                    )
-                })
-                }
-            </tbody>
-        </table>
+            <>
+                <TableHead weeks={weeks ? weeks : undefined} />
+                <tbody>
+                    {day.Exercises.map((exercise: Exercise) => {
+                        return (
+                            <TableRow
+                                key={exercise.id}
+                                id={exercise.id}
+                                name={exercise.name}
+                                series={exercise.series}
+                                reps={exercise.reps}
+                                Loads={exercise.Loads ? exercise.Loads : undefined}
+                                weeks={weeks ? weeks : undefined}
+                                routineActual={routineActual}
+                                routineId={routineId}
+                                warmUpActual={warmUpActual}
+                                warmUpId={warmUpId}
+                            />
+                        )
+                    })
+                    }
+                </tbody>
+            </>
+        </table >
     )
 }
