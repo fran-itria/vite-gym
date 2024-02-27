620a782e28bb4b736fa36b642f7e369e492a2894
fran-itria <francoitria01@gmail.com>
2024-02-27 20:52:03 -0300

    Use Meals component

--- src/components/Health/Health.tsx
+++ src/components/Health/Health.tsx
@@ -1,24 +1,16 @@
-import { useState } from "react";
-import { user } from "../../mock";
-import style from "./Health.module.css";
 import NavHealth from "./NavHealth";
 import { InformationEnum } from "../../types";
+import { useState } from "react";
+import Meals from "./Meals/Meals";
 
 export default function Health() {
-  const [information, setInformation] = useState<InformationEnum>(
-    InformationEnum.meal
-  );
+  const [information, setInformation] = useState<InformationEnum>(InformationEnum.meal)
+
   return (
     <>
       <NavHealth information={information} setInformation={setInformation} />
       {information == InformationEnum.meal ? (
-        <>
-          <ul className={style.days}>
-            {user.health.comidas.map((comida) => {
-              return <button> Dia {comida.fecha}</button>;
-            })}
-          </ul>
-        </>
+        <Meals />      
       ) : (
         <></>
       )}
