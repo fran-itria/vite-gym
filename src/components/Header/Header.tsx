953ab60ccedcbaf9c4f8f05e1d39b9e904d74b08
fran-itria <francoitria01@gmail.com>
2024-02-28 08:23:24 -0300

    Use Gym

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,20 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import style from './Header.module.css'
 import { useAppSelector } from "../../hook/store";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState } from "react";
 import { logout } from "../../services/logout/logout";
 import HomeAdmin from "../Admin/Home/HomeAdmin";
 
 export default function Header() {
     const { pathname } = useLocation()
-    const { GymId, name, surname, id, admin } = useAppSelector(state => state.user)
+    const { name, surname, id, admin, Gym} = useAppSelector(state => state.user)
     const [menu, setMenu] = useState<boolean>(false)
-    const [gymName, setGymName] = useState<string>('')
     const navigate = useNavigate()
 
-    useEffect(() => {
-        axios.get(`/gym/getGymId/${GymId}`).then(response => {
-            setGymName(response.data)
-        })
-    }, [GymId])
     return (
         <>
             <header className={style.header}>
-                <h1 className={style.gymName}>{gymName}</h1>
+                <h1 className={style.gymName}>{Gym?.name}</h1>
                 {/* <img alt="Foto" className={style.perfil} /> */}
                 <div className={style.perfil}>
                     <button onClick={() => setMenu(!menu)} className={style.photo}>{name && surname ? name[0] + surname[0] : 'P'}</button>
