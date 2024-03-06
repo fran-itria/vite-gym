7df0ae761924559a599aa09cd6c175721693f2f7
Franco Itria <francoitria01@gmail.com>
2024-03-06 16:50:53 -0300

    Subscription admin component

--- src/components/Admin/Subscription/SubscriptionAdmin.tsx
+++ src/components/Admin/Subscription/SubscriptionAdmin.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import { useEffect, useState } from "react"
 import subscription from "../../../services/subscription/subscription"
@@ -7,35 +8,46 @@ import TablePayments from "./Table"
 
 
 
-export default function SubscriptionAdmin(){
+export default function SubscriptionAdmin() {
     const { GymId } = useAppSelector(state => state.user)
     const [link, setLink] = useState<string>()
-    const [payments, setPayments] = useState<{id: string, date: string, hour: string, amount: string, User: {name: string, surname: string}}[]>()
+    const [payments, setPayments] = useState<{ id: string, date: string, hour: string, amount: string, User: { name: string, surname: string } }[]>()
     const [linkMp, setLinkMp] = useState<string>()
+    const [amount, setAmount] = useState<number>()
+
     useEffect(() => {
         axios.get(`/gym/getGymId/${GymId}`)
-        .then(response => {
-            setLinkMp(response.data.linkMp)
-            setPayments(response.data.Payments)
-        })
-        .catch(error => console.log(error))
-    },[])
+            .then(response => {
+                setLinkMp(response.data.linkMp)
+                setAmount(response.data.amount)
+                setPayments(response.data.Payments)
+            })
+            .catch(error => console.log(error))
+    }, [])
 
     return (
         <>
             <form onSubmit={(e) => {
-                if(GymId && link) subscription({e, link, id: GymId})
+                if (GymId && link) subscription({ e, link, id: GymId, setLinkMp, amount, setAmount })
             }}>
                 <label>
-                    Link de pago: 
+                    Monto:
+                    <input type="number" required onChange={(e) => setAmount(Number(e.target.value))}></input>
+                </label>
+                <label>
+                    Link de pago:
                     <input type="text" required onChange={(e) => setLink(e.target.value)}></input>
                 </label>
                 <button>Confrimar</button>
             </form>
-            {linkMp ? <p>Link de pago actual: <b> {linkMp} </b> </p> : <></>}
-            {payments && payments?.length > 0 ? 
-                <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2%'}}>
-                    <TablePayments payments={payments}/>
+            {linkMp && amount ? (
+                <>
+                    <p>Link de pago actual: <b> {linkMp} </b> </p>
+                    <p>Monto a pagar: <b>{amount}</b></p>
+                </>) : <></>}
+            {payments && payments?.length > 0 ?
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2%' }}>
+                    <TablePayments payments={payments} />
                 </div>
                 :
                 <p>No hay pagos registrados</p>
