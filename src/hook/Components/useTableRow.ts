e9a7cc2c6ccd74fbafb773f515dec538701c6c32
Franco Itria <francoitria01@gmail.com>
2024-03-08 20:27:08 -0300

    update load state

--- src/hook/Components/useTableRow.ts
+++ src/hook/Components/useTableRow.ts
@@ -4,6 +4,22 @@ export default function useTabelRow() {
     const [open, setOpen] = useState<boolean>(false)
     const [openLoad, setOpenLoad] = useState<boolean>(false)
     const [confirmDelete, setConfirmDelete] = useState<boolean>(false)
+    const [load, setLoad] = useState<boolean>(false)
+    const [idLoad, setIdLoad] = useState<string>('')
+    const [newLoads, setNewLoads] = useState<string>('')
 
-    return { open, setOpen, openLoad, setOpenLoad, confirmDelete, setConfirmDelete }
+    return {
+        open,
+        setOpen,
+        openLoad,
+        setOpenLoad,
+        confirmDelete,
+        setConfirmDelete,
+        load,
+        setLoad,
+        idLoad,
+        setIdLoad,
+        newLoads,
+        setNewLoads
+    }
 }
