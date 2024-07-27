936fdf8837e0722edec762562e0595cfa59dcde2
Franco Itria <francoitria01@gmail.com>
2024-07-27 01:26:31 -0300

    Custom hook

---
+++ src/components/Session/register/useForm.ts
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { InputsLogin, InputsRegister } from "../../../types";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { getId } from "../../../services/getId";
+import { useUserActions } from "../../../hook/useUserActions";
+import useLoaders from "../../../hook/Components/useLoaders";
+
+export default function useForm() {
+    const [inputs, setInputs] = useState<InputsRegister | InputsLogin>()
+    const url = useLocation()
+    const params = useParams()
+    const navigate = useNavigate()
+    const { addUser } = useUserActions()
+    const { loader, setLoader } = useLoaders()
+
+    const [open, setOpen] = useState(false)
+    const [mail, setMail] = useState('')
+    const [temporalCode, setTemporalCode] = useState('')
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    useEffect(() => {
+        const { id } = params
+        getId(id, navigate)
+        const gymName = params.gymName
+        if (gymName) {
+            setInputs(prevInputs => { return { ...prevInputs, gymName } })
+        }
+    }, [])
+
+    return { inputs, setInputs, url, navigate, addUser, loader, setLoader, open, handleOpen, mail, setMail, temporalCode, setTemporalCode }
+}
