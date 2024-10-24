d773296f0c177b82f732a9e5dd5e6ce742ab0865
fran-itria <francoitria01@gmail.com>
2024-10-24 02:00:43 -0300

    Added Modal

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,11 @@
+import { Modal } from "@mui/material";
 
 export default function Loader({ text }: { text: string }) {
     return (
-        <div className='w-screen h-screen flex justify-center items-center text-center'>
-            <p className='animate-bounce text-white text-4xl rounded-lg border p-2 bg-gray-700 dark:bg-cyan-950'>{text}...</p>
-        </div>
+        <Modal open={Boolean(text)}>
+            <div className='w-screen h-screen flex justify-center items-center text-center'>
+                <p className='animate-bounce text-white text-4xl rounded-lg border p-2 bg-gray-700 dark:bg-cyan-950'>{text}...</p>
+            </div>
+        </Modal>
     )
 }
