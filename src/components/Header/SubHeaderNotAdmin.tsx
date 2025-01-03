16cf69e18ff94db651c3aaabfae039f89aeced49
fran-itria <francoitria01@gmail.com>
2025-01-03 02:33:50 -0300

    Style navbar not admin

--- src/components/Header/SubHeaderNotAdmin.tsx
+++ src/components/Header/SubHeaderNotAdmin.tsx
@@ -9,26 +9,60 @@ export default function SubHeaderNotAdmin({
 }: props) {
 
     const { pathname } = useLocation()
+    const styleNavButton = 'p-1 w-32 ll:h-fit ll:text-sm'
 
     return (
         <nav>
-            <ul>
-                <NavLink to={`/home/${id}/resumen`} className={pathname.includes("home") ? '' : ''}>
+            <ul className="
+                flex
+                justify-evenly
+                w-full
+                ll:flex-wrap
+                ll:gap-2"
+            >
+                <NavLink
+                    to={`/home/${id}/resumen`}
+                    className={pathname.includes("home")
+                        ?
+                        `rounded bg-gray-300  ${styleNavButton}`
+                        :
+                        `button ${styleNavButton}`}>
                     <li>
                         Inicio
                     </li>
                 </NavLink>
-                <NavLink to={`/calentamiento/${id}`} className={({ isActive }) => isActive ? '' : ''}>
+                <NavLink
+                    to={`/calentamiento/${id}`}
+                    className={({ isActive }) => isActive
+                        ?
+                        `rounded bg-gray-300  ${styleNavButton}`
+                        :
+                        `button ${styleNavButton}`}
+                >
                     <li>
                         Calentamiento
                     </li>
                 </NavLink>
-                <NavLink to={`/rutina/${id}`} className={({ isActive }) => isActive ? '' : ''}>
+                <NavLink
+                    to={`/rutina/${id}`}
+                    className={({ isActive }) => isActive
+                        ?
+                        `rounded bg-gray-300  ${styleNavButton}`
+                        :
+                        `button ${styleNavButton}`}
+                >
                     <li>
                         Rutina
                     </li>
                 </NavLink>
-                <NavLink to={`/suscripcion`} className={({ isActive }) => isActive ? '' : ''}>
+                <NavLink
+                    to={`/suscripcion`}
+                    className={({ isActive }) => isActive
+                        ?
+                        `rounded bg-gray-300  ${styleNavButton}`
+                        :
+                        `button ${styleNavButton}`}
+                >
                     <li>
                         Suscripción
                     </li>
