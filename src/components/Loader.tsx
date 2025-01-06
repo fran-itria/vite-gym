8c3f860a1f4013ab8132e0830ba9d37c9e148c2d
fran-itria <francoitria01@gmail.com>
2025-01-06 03:05:58 -0300

    Adjusted width

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,8 +2,8 @@ import { Modal } from "@mui/material";
 
 export default function Loader({ text }: { text: string }) {
     return (
-        <Modal open={Boolean(text)}>
-            <div className='w-screen h-screen flex justify-center items-center text-center'>
+        <Modal open={Boolean(text)} className="flex justify-center items-center">
+            <div className='w-80 h-screen flex justify-center items-center text-center'>
                 <p className='animate-bounce text-white text-4xl rounded-lg border p-2 bg-gray-700 dark:bg-cyan-950'>{text}...</p>
             </div>
         </Modal>
