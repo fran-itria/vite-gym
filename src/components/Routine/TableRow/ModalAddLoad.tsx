435d2b556afb77c6d293e8477ff943b0d9f075cf
fran-itria <francoitria01@gmail.com>
2025-01-08 03:29:51 -0300

    Load is required

--- src/components/Routine/TableRow/ModalAddLoad.tsx
+++ src/components/Routine/TableRow/ModalAddLoad.tsx
@@ -12,10 +12,23 @@ export default function ModalAddLoad({
   const [inputLoad, setInputLoad] = useState<string>("");
 
   return (
-    <div className="background flex flex-col p-4 rounded">
+    <form
+      className="background flex flex-col p-4 rounded"
+      onSubmit={(e) => modifiedLoads({
+        e,
+        exerciseId: id,
+        load: inputLoad,
+        routineId: routineOrWarmUp.routineId,
+        routineActual: routineOrWarmUp.routineActual,
+        setOpenLoad,
+        setLoader,
+        weekLoad,
+      })}
+    >
       <input
         autoFocus
         placeholder="Cargar peso"
+        required
         onChange={(e) => setInputLoad(e.target.value)}
       ></input>
       <p className="w-52 mt-4 font-bold text-gray-900 dark:text-white text-wrap text-center">
@@ -24,27 +37,17 @@ export default function ModalAddLoad({
       <div className="flex justify-between mt-4">
         <button
           className="buttonCancel w-24"
+          type="button"
           onClick={() => setOpenLoad((openLoad) => !openLoad)}
         >
           Cancelar
         </button>
         <button
           className="buttonConfirm w-24"
-          onClick={() =>
-            modifiedLoads({
-              exerciseId: id,
-              load: inputLoad,
-              routineId: routineOrWarmUp.routineId,
-              routineActual: routineOrWarmUp.routineActual,
-              setOpenLoad,
-              setLoader,
-              weekLoad,
-            })
-          }
         >
           Agregar
         </button>
       </div>
-    </div>
+    </form>
   );
 }
--- src/components/Routine/TableRow/ModifiedLoad.tsx
+++ src/components/Routine/TableRow/ModifiedLoad.tsx
@@ -16,39 +16,41 @@ type ModifiedLoadProps = {
 
 export default function ModifiedLoad({ setNewLoads, setLoad, setLoader, id, load, routineActual, routineId, currentLoad }: ModifiedLoadProps) {
     return (
-        <div className="background rounded p-4 flex flex-col">
+        <form
+            className="background rounded p-4 flex flex-col"
+            onSubmit={(e) => modifiedLoads({
+                e,
+                id,
+                load,
+                routineActual,
+                routineId,
+                setLoad,
+                setLoader,
+            })}
+        >
             <input
                 type="text"
                 placeholder="Carga"
+                required
                 onChange={(e) => setNewLoads(e.target.value)}
                 defaultValue={currentLoad}
             ></input>
             <div className="flex justify-between mt-3">
                 <button
-                    className="buttonConfirm w-24"
-                    onClick={() =>
-                        modifiedLoads({
-                            id,
-                            load,
-                            routineActual,
-                            routineId,
-                            setLoad,
-                            setLoader,
-                        })
-                    }
-                >
-                    Guardar
-                </button>
-                <button
                     className='buttonCancel w-24'
+                    type="button"
                     onClick={() => {
-                        console.log('cancel')
                         setLoad(undefined)
                     }}
                 >
                     Cancelar
                 </button>
+                <button
+                    className="buttonConfirm w-24"
+                >
+                    Guardar
+                </button>
             </div>
-        </div>
+        </form>
     )
 }
